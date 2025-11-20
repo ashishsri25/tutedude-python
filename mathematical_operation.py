de6decfad3864860f9dd6779e4c304c5a8827de0
ashishsri25 <28553697+ashishsri25@users.noreply.github.com>
2025-11-20 09:19:06 +0530

    assignment 1

---
+++ mathematical_operation.py
@@ -0,0 +1,14 @@
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+# Addition
+add = n1 + n2
+print("Addition:",add)
+# substraction
+sub = n1 -n2
+print("Substraction:",sub)
+# multiplication
+mul = n1 * n2
+print("Multiplication:",mul)
+# division
+div = n1/n2
+print("Division:",div)
